7cddba2a08ba837f42b7c6414f673dd4aeb263b4
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-20 14:21:14 +0200

    added show/hide on scroll to poster

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,23 @@
 showElement = document.getElementById("placeholder");
+showPoster = document.getElementById("poster");
 
 var myScrollFunc = function () {
   var y = window.scrollY;
-  if (y >= 500) {
-    showElement.className = "placeholder show"
+  if (y >= 1400) {
+    showElement.className = "placeholder show";
   } else {
-    showElement.className = "placeholder hide"
+    showElement.className = "placeholder hide";
+  }
+};
+
+var opacityScroll = function () {
+  var y = window.scrollY;
+  if (y >= 2300) {
+    showPoster.className = "visible";
+  } else {
+    showPoster.className = "hidden";
   }
 };
 
 window.addEventListener("scroll", myScrollFunc);
+window.addEventListener("scroll", opacityScroll);
