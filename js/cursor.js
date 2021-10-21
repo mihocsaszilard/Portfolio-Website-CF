4bea350471d6dd1ffcfedafcf181a401f9812a49
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-21 17:56:24 +0200

    added to top button + restyled cursor

--- js/cursor.js
+++ js/cursor.js
@@ -22,12 +22,12 @@ function mouseMoveHandler(e) {
   if (e.target.classList.contains("link")) {
     cursor.style.transform = "scale(3)";
     lazy.style.transform = "scale(8)";
-    lazy.style.background = "rgb(255, 154, 60)";
+    lazy.style.background = "rgb(55, 187, 180)";
 
-    cursor.style.boxShadow = "0 0 5px rgb(255, 154, 60)";
+    cursor.style.boxShadow = "0 0 5px rgb(55, 187, 180)";
   } else {
     cursor.style.boxShadow = "none";
-    lazy.style.background = "rgba(255, 154, 60, 0.8)";
+    lazy.style.background = "rgba(55, 187, 180, 0.8)";
   }
 }
 
@@ -46,10 +46,10 @@ function mouseDownHandler() {
 
 function linkEnterHandler() {
   cursor.style.transform = "scale(1.5)";
-  cursor.style.border = "2px solid rgb(255, 154, 60)";
+  cursor.style.border = "2px solid rgb(55, 187, 180)";
 }
 
 function linkLeaveHandler() {
   cursor.style.transform = "scale(1)";
-  cursor.style.border = "2px solid rgb(255, 154, 60)";
+  cursor.style.border = "2px solid rgb(55, 187, 180)";
 }
